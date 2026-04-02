bf672989eac4b0e46cd44e4a3ca934fc7708db8a
Vladimir Golubev <v@golubevs.art>
2026-04-03 00:37:39 +0300

    Generate a 52 cards deck and print random cards

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,36 @@
-fn main() {
-    const SUITS: [char; 4] = ['H', 'S', 'D', 'C'];
-    const VALUE: [i8; 13] = [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14];
+use rand::prelude::*;
 
-    struct Card {
-        suit: char,
-        value: i8,
+fn main() {
+    let deck: Vec<Card> = construct_deck();
+    for i in 1..=5 {
+        let card = get_random_card(&deck);
+        println!("Card {}: {}", i, card);
     }
+}
+
+fn construct_deck() -> Vec<Card> {
+    const SUITS: [char; 4] = ['H', 'S', 'D', 'C'];
+    const VALUE: [i8; 13] = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+    let mut deck = Vec::new();
+
+    'suits: for suit in SUITS {
+        'values: for value in VALUE {
+            deck.push(Card { suit, value })
+        }
+    }
+
+    deck
+}
+
+fn get_random_card(deck: &Vec<Card>) -> String {
+    let mut rng = rand::rng();
+    let random_index = rng.random_range(0..52);
+    let random_card: &Card = &deck[random_index];
+    format!("{}{}", random_card.suit, random_card.value)
+}
+
+struct Card {
+    suit: char,
+    value: i8,
 }
