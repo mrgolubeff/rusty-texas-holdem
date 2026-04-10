5f53acc3c3aeafab5a99d2fc3212aaddbf20c7a7
Vladimir Golubev <v@golubevs.art>
2026-04-10 17:14:09 +0300

    Make a card module for my card game

---
+++ src/card.rs
@@ -0,0 +1,45 @@
+pub enum Suit {
+    Hearts,
+    Spades,
+    Diamonds,
+    Clubs,
+}
+
+pub struct Card {
+    suit: Suit,
+    value: u8,
+    // "is_ace" field's needed so that calling code can check
+    // if it should combinations with both "1" and "14" values.
+    is_ace: bool,
+}
+
+impl Card {
+    pub fn new(suit: Suit, value: u8) -> Self {
+        let is_ace: bool;
+        if value == 14 {
+            is_ace = true;
+        } else {
+            is_ace = false;
+        }
+
+        Self {
+            suit,
+            value,
+            is_ace,
+        }
+    }
+}
+
+impl Card {
+    pub fn get_value(&self) -> u8 {
+        self.value
+    }
+
+    pub fn get_suit(&self) -> &Suit {
+        &self.suit
+    }
+
+    pub fn is_ace(&self) -> bool {
+        self.is_ace
+    }
+}
