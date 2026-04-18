165a3ea23f36a51cdb8741e1ba03270768a4d7c8
Vladimir Golubev <v@golubevs.art>
2026-04-18 16:24:25 +0300

    Create an enum for poker hands

---
+++ src/hands.rs
@@ -0,0 +1,12 @@
+pub enum Hands {
+    RoyalFlush(u16),
+    StraightFlush(u16),
+    FourOfAKind(u16),
+    FullHouse(u16),
+    Flush(u16),
+    Straight(u16),
+    ThreeOfAKind(u16),
+    TwoPair(u16),
+    Pair(u16),
+    Kicker(u16),
+}
