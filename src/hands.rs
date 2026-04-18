a33986003bc48c3c8989c4f5d1bea4ae4380bf99
Vladimir Golubev <v@golubevs.art>
2026-04-18 17:57:49 +0300

    Add a method to extract value from Hands variants

--- src/hands.rs
+++ src/hands.rs
@@ -10,3 +10,21 @@ pub enum Hands {
     Pair(u16),
     HighCard(u16),
 }
+
+impl Hands {
+    // retrieve value from any of the variants.
+    pub fn get_score(&self) -> u16 {
+        match self {
+            Self::RoyalFlush(value) |
+            Self::StraightFlush(value) |
+            Self::FourOfAKind(value) |
+            Self::FullHouse(value) |
+            Self::Flush(value) |
+            Self::Straight(value) |
+            Self::ThreeOfAKind(value) |
+            Self::TwoPair(value) |
+            Self::Pair(value) |
+            Self::HighCard(value) => *value
+        }
+    }
+}
