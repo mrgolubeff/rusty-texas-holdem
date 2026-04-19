3a0c9562c849f7e8f07225bccd997e8d25d68324
Vladimir Golubev <v@golubevs.art>
2026-04-19 11:32:24 +0300

    Rearrange Or operators to look more pleasing

--- src/hands.rs
+++ src/hands.rs
@@ -17,16 +17,16 @@ impl Hand {
     // retrieve value from any of the variants.
     pub fn get_score(&self) -> u16 {
         match self {
-            Self::RoyalFlush(value) |
-            Self::StraightFlush(value) |
-            Self::FourOfAKind(value) |
-            Self::FullHouse(value) |
-            Self::Flush(value) |
-            Self::Straight(value) |
-            Self::ThreeOfAKind(value) |
-            Self::TwoPair(value) |
-            Self::Pair(value) |
-            Self::HighCard(value) => *value
+            Self::RoyalFlush(value)
+            | Self::StraightFlush(value)
+            | Self::FourOfAKind(value)
+            | Self::FullHouse(value)
+            | Self::Flush(value)
+            | Self::Straight(value)
+            | Self::ThreeOfAKind(value)
+            | Self::TwoPair(value)
+            | Self::Pair(value)
+            | Self::HighCard(value) => *value
         }
     }
 }
