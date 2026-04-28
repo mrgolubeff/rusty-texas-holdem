a5ff7267c2de26b088e08e2478410befaafcaedf
Vladimir Golubev <inbox@golubevs.art>
2026-04-28 18:56:48 +0300

    Clear the main() for now

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,1 @@
-use rusty_holdem::deck::Deck;
-
-fn main() {
-    let mut deck = Deck::construct_deck();
-    println!("{:?}\n\n\n", deck);
-    deck.shuffle_deck();
-    println!("{:?}", deck);
-}
+fn main() {}
