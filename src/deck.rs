2b1c31572f1fb07ad2fab1ff66a90d8bdfd24b01
Vladimir Golubev <inbox@golubevs.art>
2026-05-08 23:58:22 +0300

    Simplify deck creation logic via SUITS array

--- src/deck.rs
+++ src/deck.rs
@@ -13,22 +13,16 @@ impl Deck {
     pub fn construct_deck() -> Self {
         let mut cards = Vec::new();
 
-        'suits: for suit_number in 0..4 {
-            'values: for value in 2..=14 {
-                let suit: Suit = match suit_number {
-                    0 => Suit::Hearts,
-                    1 => Suit::Spades,
-                    2 => Suit::Diamonds,
-                    3 => Suit::Clubs,
-                    val => panic!("suit_number should be 0, 1, 2 or 3, but value is {val}.")
-                };
-                cards.push(
-                    Card::new(suit, value)
-                );
+        for suit in SUITS {
+            for value in 2..=14 {
+                cards.push(Card::new(suit.clone(), value));
             }
         }
 
-        Self { cards, potato: rng() }
+        Self {
+            cards,
+            potato: rng(),
+        }
     }
 }
 
